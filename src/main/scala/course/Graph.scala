56e7fb608e52780c0994be17d68312f800e9dee9
s@lamandra <ussashok09@gmail.com>
2020-02-22 13:33:02 -0800

    Refactoring

--- src/main/scala/course/Graph.scala
+++ src/main/scala/course/Graph.scala
@@ -40,7 +40,7 @@ trait Graph[V] {
 
   def traversalDfs(start: V): Option[List[V]] = dfs(start, _ => false, x => if (x.isEmpty) None else Some(x))
 
-  private def dfsRec(start: V, finishCondition: V => Boolean, transformAllVisited: List[V] => Option[List[V]], merge: (V, List[V]) => Option[List[V]]): Option[List[V]] = {
+  private def dfsRec(start: V, finishCondition: V => Boolean, transformAllVisited: List[V] => Option[List[V]], isFirstResult: Boolean): Option[List[V]] = {
 
     def dfsRec(nowVisit: V, visited: List[V]): Option[List[V]] = {
       if (visited.contains(nowVisit)) {
@@ -52,7 +52,7 @@ trait Graph[V] {
         neighbours(nowVisit).foldLeft(Option.empty[List[V]]) {
           case (result, neighbour) => result match {
             case None => dfsRec(neighbour, visitedZero)
-            case Some(visitedNow) => merge(neighbour, visitedNow)
+            case Some(visitedNow) => if (isFirstResult) Some(visitedNow) else dfsRec(neighbour, visitedNow)
           }
         }
       }
@@ -62,46 +62,9 @@ trait Graph[V] {
     dfsRec(start, Nil).map(_.reverse)
   }
 
-  def dfsRec(start: V, end: V): Option[List[V]] = {
+  def dfsRec(start: V, end: V): Option[List[V]] = dfsRec(start, v => v == end, _ => None, isFirstResult = true)
 
-    def dfsRec(nowVisit: V, visited: List[V]): Option[List[V]] = {
-      if (visited.contains(nowVisit)) {
-        None
-      } else if (nowVisit == end) {
-        Some(nowVisit :: visited)
-      } else {
-        val visitedZero = nowVisit :: visited
-        neighbours(nowVisit).foldLeft(Option.empty[List[V]]) {
-          case (result, neighbour) => result match {
-            case None => dfsRec(neighbour, visitedZero)
-            case some => some
-          }
-        }
-      }
-
-    }
-
-    dfsRec(start, Nil).map(_.reverse)
-  }
-
-  def traversalDfsRec(start: V): Option[List[V]] = {
-    def traversalDfsRec(nowVisit: V, visited: List[V]): Option[List[V]] = {
-      if (visited.contains(nowVisit)) {
-        Some(visited)
-      } else {
-        val neighboursNow = neighbours(nowVisit)
-        val visitedZero = nowVisit :: visited
-        neighboursNow.foldLeft(Option.empty[List[V]]) {
-          case (result, neighbour) => result match {
-            case None => traversalDfsRec(neighbour, visitedZero)
-            case Some(visitedNow) => traversalDfsRec(neighbour, visitedNow)
-          }
-        }
-      }
-    }
-
-    traversalDfsRec(start, Nil).map(_.reverse)
-  }
+  def traversalDfsRec(start: V): Option[List[V]] = dfsRec(start, _ => false, x => if (x.isEmpty) None else Some(x), isFirstResult = false)
 
   def bfs(start: V, end: V): Option[List[V]] = {
 
